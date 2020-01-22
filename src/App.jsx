3c174bba0d21b36dff9a9fd4c5abe2b723a525ad
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-01-22 22:35:23 +0300

    refactor(components): change structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from 'components/Header/Header';
+import Header from 'components/Header/Index';
 
 import Map from 'views/Map';
 import Login from 'views/Login';
