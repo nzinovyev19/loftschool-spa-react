c3ef6d842e38cc48e981666ecf4fe71ce7c2439a
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-01-26 01:13:45 +0300

    feat(components): add Theme and ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from 'components/Header/Index';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from 'loft-taxi-mui-theme';
 
 import Map from 'views/Map';
 import Login from 'views/Login';
@@ -7,7 +8,7 @@ import Profile from 'views/Profile';
 import Registration from 'views/Registration';
 
 const views = {
-  map: function map() { return <Map />; },
+  map: function map(setPage) { return <Map setPage={setPage} />; },
   login: function login(setPage) { return <Login setPage={setPage} />; },
   profile: function profile() { return <Profile />; },
   registration: function registration(setPage) { return <Registration setPage={setPage} />; },
@@ -17,10 +18,11 @@ function App() {
   const [page, setPage] = React.useState('login');
 
   return (
-    <div className="App">
-      <Header setPage={setPage} />
-      {views[page](setPage)}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {views[page](setPage)}
+      </div>
+    </ThemeProvider>
   );
 }
 
