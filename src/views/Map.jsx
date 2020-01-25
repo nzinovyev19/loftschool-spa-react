d826cb4fb9d80567943798cde2c8afd7999fc0f4
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-01-26 01:12:45 +0300

    feat(components): create map page

--- src/views/Map.jsx
+++ src/views/Map.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Mapboxgl from 'mapbox-gl';
+import Header from 'components/Header/Index';
 
-export default function Map() {
+Map.propTypes = {
+  setPage: PropTypes.func.isRequired,
+};
+
+export default function Map({ setPage }) {
+  const [mapOptions] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2,
+  });
+  const style = {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: '100%',
+  };
+  let mapContainer;
+  Mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+  useEffect(() => {
+    const map = new Mapboxgl.Map({
+      container: mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [mapOptions.lng, mapOptions.lat],
+      zoom: mapOptions.zoom,
+    });
+    return () => {
+      map.remove();
+    };
+  });
+
   return (
-    <div>Map</div>
+    <>
+      <Header setPage={setPage} />
+      <div style={style} ref={(el) => { mapContainer = el; }} />
+    </>
   );
 }
