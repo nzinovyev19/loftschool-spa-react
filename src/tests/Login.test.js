a9366772bfdd3bd91261a3ca54c75875d2dee2b9
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-01-28 23:21:10 +0300

    test(components): header, login, registration components test created

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import Login from 'views/Login';
+import { render, fireEvent } from '@testing-library/react';
+import LoginForm from 'components/LoginForm/Index';
 
 describe('Header.jsx', () => {
-  test('renders buttons value', () => {
-    const { getAllByText } = render(<Login setPage={() => true} />);
-    expect(getAllByText(/Войти/i)).toHaveLength(2);
+  const setPageFunc = jest.fn();
+
+  it('change email input value', () => {
+    const { getByPlaceholderText } = render(<LoginForm setPage={setPageFunc} />);
+    const emailInput = getByPlaceholderText('Почта*');
+
+    fireEvent.change(emailInput, { target: { value: 'test' } });
+
+    expect(emailInput.value).toBe('test');
+  });
+  it('change password input value', () => {
+    const { getByPlaceholderText } = render(<LoginForm setPage={setPageFunc} />);
+    const emailInput = getByPlaceholderText('Пароль*');
+
+    fireEvent.change(emailInput, { target: { value: 'test' } });
+
+    expect(emailInput.value).toBe('test');
+  });
+  it('renders button for login', () => {
+    const { getByTestId } = render(<LoginForm setPage={setPageFunc} />);
+    expect(getByTestId('login-btn')).toBeTruthy();
+  });
+  it('trigger setPageFunc on button click', () => {
+    const { getByTestId } = render(<LoginForm setPage={setPageFunc} />);
+    const buttonRegistrate = getByTestId('registration-link');
+
+    fireEvent.click(buttonRegistrate);
+
+    expect(setPageFunc).toHaveBeenCalled();
   });
 });
--- src/tests/Registration.test.js
+++ src/tests/Registration.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Registration from 'views/Registration';
 
 describe('Header.jsx', () => {
-  test('renders buttons value', () => {
-    const { getByText } = render(<Registration setPage={() => true} />);
+  const setPageFunc = jest.fn();
+
+  it('renders buttons value', () => {
+    const { getByText } = render(<Registration setPage={setPageFunc} />);
     expect(getByText(/Регистрация/i)).toBeInTheDocument();
+  });
+
+  it('trigger setPageFunc on button click', () => {
+    const { getByTestId } = render(<Registration setPage={setPageFunc} />);
+    const buttonRegistrate = getByTestId('login-link');
+
+    fireEvent.click(buttonRegistrate);
+
+    expect(setPageFunc).toHaveBeenCalled();
   });
 });
