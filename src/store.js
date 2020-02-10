267d9f89a6a37be603d5d480291fe39166fdb031
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-02-10 23:33:27 +0300

    feat(components): add and init saga-redux library

--- src/store.js
+++ src/store.js
@@ -1,20 +1,23 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import authMiddleware from 'modules/auth/middlewares';
-import profileMiddleware from 'modules/profile/middlewares';
-import rootReducer from 'modules/';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer, { rootSaga } from 'modules/';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const createAppStore = () => {
   const store = createStore(
     rootReducer,
     compose(
-      applyMiddleware(authMiddleware),
-      applyMiddleware(profileMiddleware),
+      applyMiddleware(sagaMiddleware),
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
   );
   const persistor = persistStore(store);
+  sagaMiddleware.run(rootSaga);
   return { store, persistor };
 };
+
 
 export default createAppStore;
