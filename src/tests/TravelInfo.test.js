27e124a06290f69b9e6c58012a898217e9858527
Nikita Zinovev <nikita.z@rcsmail.ru>
2020-02-18 19:03:52 +0300

    test(components): change tests for TravelInfo component, add wrap function redux

--- src/tests/TravelInfo.test.js
+++ src/tests/TravelInfo.test.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react';
-import TravelInfo from 'components/MapInfo/TravelInfo';
 
+import TravelInfo from 'components/MapInfo/TravelInfo';
+import rootReducer from 'modules/';
+
+function renderWithRedux(ui, { initialState, store = createStore(rootReducer, initialState) } = {}) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store,
+  };
+}
 
 describe('ProfilInfo.jsx', () => {
   describe('Select input values', () => {
+    const store = createStore(() => ({
+      addresses: {
+        error: null,
+        isLoading: false,
+        addresses: [],
+      },
+    }));
     it('Change select "from"', () => {
-      const { getByPlaceholderText } = render(<TravelInfo />);
+      const { getByPlaceholderText } = renderWithRedux(<TravelInfo />, store);
       const select = getByPlaceholderText('Откуда');
 
       fireEvent.change(select, { target: { value: 'Пушкина' } });
@@ -14,7 +31,7 @@ describe('ProfilInfo.jsx', () => {
       expect(select.value).toBe('Пушкина');
     });
     it('Change select "to"', () => {
-      const { getByPlaceholderText } = render(<TravelInfo />);
+      const { getByPlaceholderText } = render(<Provider store={store}><TravelInfo /></Provider>);
       const select = getByPlaceholderText('Куда');
 
       fireEvent.change(select, { target: { value: 'Пушкина' } });
